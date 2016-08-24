695c68abab08c0032154158c161821666722ddfc
Martin Stone <martin@d7415.co.uk>
2016-08-24 21:46:03 +0100

    Remove more unused files.

--- dump_db.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mysqldump -xuroot -p merlin > mysql-merlin-`date +"%Y%m%d.%H%M%S"`_$1.sql
--- many_ticks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in {285..311}
-do
-   echo "Tick $i"
-   perl -pi -w -e "s/tk\/$(($i-1))/tk\/$i/g;" merlin.cfg
-   python excalibur.py
-done
